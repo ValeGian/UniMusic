8b8aa1dce0100f1a3cc5653351b65b75e963404c
Valerio Giannini <46gianninivalerio@gmail.com>
2020-12-07 16:54:22 +0100

    Lavorato su User DAO

--- UniMusic/src/main/java/it/unipi/dii/inginf/lsmdb/unimusic/middleware/entities/PrivilegeLevel.java
+++ UniMusic/src/main/java/it/unipi/dii/inginf/lsmdb/unimusic/middleware/entities/PrivilegeLevel.java
@@ -1,6 +1,15 @@
 package it.unipi.dii.inginf.lsmdb.unimusic.middleware.entities;
 
 public enum PrivilegeLevel {
-    STANDARD_USER,
-    ADMIN
+    STANDARD_USER("standard_user"),
+    ADMIN("admin");
+
+    private String privilegeLevelName;
+    private PrivilegeLevel(String privilegeLevelName) {
+        this.privilegeLevelName = privilegeLevelName;
+    }
+
+    public String toString() {
+        return privilegeLevelName;
+    }
 }
--- UniMusic/src/main/java/it/unipi/dii/inginf/lsmdb/unimusic/middleware/exception/ActionNotCompletedException.java
+++ UniMusic/src/main/java/it/unipi/dii/inginf/lsmdb/unimusic/middleware/exception/ActionNotCompletedException.java
@@ -1,8 +1,33 @@
 package it.unipi.dii.inginf.lsmdb.unimusic.middleware.exception;
 
 public class ActionNotCompletedException extends Exception{
+    private static final long serialVersionUID = 7718828512143293558L;
 
-    public ActionNotCompletedException() { super("Error: Action not completed"); }
+    private int code;
+
+    public ActionNotCompletedException(int code) {
+        super();
+        this.code = code;
+    }
+
+    public ActionNotCompletedException(String message, Throwable cause, int code) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public ActionNotCompletedException(String message, int code) {
+        super(message);
+        this.code = code;
+    }
+
+    public ActionNotCompletedException(Throwable cause, int code) {
+        super(cause);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
 
     public ActionNotCompletedException(String message) {
         super(message);
@@ -15,4 +40,5 @@ public class ActionNotCompletedException extends Exception{
     public ActionNotCompletedException(String message, Throwable throwable) {
         super(message, throwable);
     }
+
 }
