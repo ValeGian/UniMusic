3995cd6b10a96dfde9abd8539e6b5413bc6b007f
lorenzo <l.bianchi45@studenti.unipi.it>
2020-12-19 19:30:37 +0100

    Aggiunto campo country al momento della registrazione degli utenti

--- UniMusic/src/main/java/it/unipi/dii/inginf/lsmdb/unimusic/frontend/gui/welcomeController.java
+++ UniMusic/src/main/java/it/unipi/dii/inginf/lsmdb/unimusic/frontend/gui/welcomeController.java
@@ -2,11 +2,14 @@ package it.unipi.dii.inginf.lsmdb.unimusic.frontend.gui;
 
 import it.unipi.dii.inginf.lsmdb.unimusic.frontend.MiddlewareConnector;
 import it.unipi.dii.inginf.lsmdb.unimusic.middleware.exception.ActionNotCompletedException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
@@ -29,10 +32,16 @@ public class welcomeController implements Initializable {
     @FXML private TextField regPassword;
     @FXML private Button registerButton;
     @FXML private TextField registerMessage;
+    @FXML private ComboBox countryBox;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         connector = MiddlewareConnector.getInstance();
+
+        String[] countries = {"Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua-Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia-Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre-Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts-Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad-Tobago","Tunisia","Turkey","Turkmenistan","Turks-Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"};
+        ObservableList<String> countriesObservable = FXCollections.observableArrayList(countries);
+
+        countryBox.setItems(countriesObservable);
 
         regAge.setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
@@ -75,19 +84,21 @@ public class welcomeController implements Initializable {
                 String firstName = regFirstName.getText();
                 String lastName = regLastName.getText();
                 String age = regAge.getText();
-
+                String country;
                 clear();
 
                 if(username.equals("")
                 || password.equals("")
                 || firstName.equals("")
                 || lastName.equals("")
-                ||age.equals("")) {
+                || age.equals("")
+                || countryBox.getSelectionModel().isEmpty()) {
                     registerMessage.setStyle("-fx-text-fill: red; -fx-background-color: transparent");
                     registerMessage.setText("You have to fill all the fields!");
                 } else {
                     try {
-                        connector.registerUser(username, password, firstName, lastName, Integer.parseInt(age));
+                        country = countryBox.getValue().toString();
+                        connector.registerUser(username, password, firstName, lastName, Integer.parseInt(age), country);
                         registerMessage.setStyle("-fx-text-fill: green; -fx-background-color: transparent");
                         registerMessage.setText("You have succesfully registered!");
 
